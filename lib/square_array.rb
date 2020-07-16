8ab3b0c8ed22026af0deab75f25e445aa92562bb
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-16 00:49:32 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
 numbers = [1,2,3]
  def square_array(numbers)
-   
-   while numbers.length.times do
-     numbers * 2
-     
-   end
+ new_array = []
+
+ numbers.length do |integer|
+  new_array << integer ** 2
  end
+ return new_array
+end
